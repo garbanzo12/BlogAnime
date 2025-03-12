697a89ae3156e5567b899c6433d0a37a53de53d7
Kyrock04 <santiempresariyluna@gmail.com>
2025-03-12 18:05:35 -0500

    Commint
    Se hizo la respectiva creacion del funcionamiento del login, login ubicado en la carpeta nombrada "login-register"

--- assets/login-register/login.php
+++ assets/login-register/login.php
@@ -1,3 +1,22 @@
 <?php
     include '../includes/conexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Hash de la contraseña para mayor seguridad
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insertar datos en la base de datos
+    $sql = "INSERT INTO usuarios (email, password) VALUES ('$email', '$hashed_password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Nuevo registro creado exitosamente";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
 ?>
