eb5e16c1e9299bb6f4c5edb34830b197f5fbbb29
Esteban Patiño <gaonaestebandpg@gmail.com>
2025-03-12 15:41:18 -0500

    Primer Include - navbar.php
    Creacion del primer include. El nav bar, en php.

--- index.php
+++ index.php
@@ -1,48 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="es">
-    <head>
-        <meta charset="utf-8" />
-        <title> Esta es una prubea del tittle Blog de {Temas}</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
-    
-        
-    </head>
-    <body>
-        <!-- CABECERA -->
-        <header id="cabecera">
-            <!-- LOGO -->
-            <div id="logo">
-                <a href="index.php">
-                    Prueba DPG Commit Blog de {Temas}
-                </a>
-            </div>
-            
-            <!-- MENU -->
-            <nav id="menu">
-                <ul>
-                    <li>
-                        <a href="index.php">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="index.php">{Categorias}</a>
-                    </li>
-                    <li>
-                        <a href="index.php">{Categorias}</a>
-                    </li>
-                    <li>
-                        <a href="index.php">{Categorias}</a>
-                    </li>
-                    <li>
-                        <a href="index.php">Sobre mí</a>
-                    </li>
-                    <li>
-                        <a href="index.php">Contacto</a>
-                    </li>
-                </ul>
-            </nav>
-            
-            <div class="clearfix"></div>
-        </header>
+<?php include 'assets/includes/navbar.php';?>
         
         <div id="contenedor">
             <!-- BARRA LATERAL -->
